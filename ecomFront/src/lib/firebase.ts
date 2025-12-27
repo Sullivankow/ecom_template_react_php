3a13ff438b14279f81092e32f052d44331886d48
SunDev <sundly@live.fr>
2025-12-27 19:20:43 +0100

    update .env ok

--- ecomFront/src/lib/firebase.ts
+++ ecomFront/src/lib/firebase.ts
@@ -1,25 +1,20 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-//  Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// On récupère les clés depuis le fichier .env (Vite utilise import.meta.env)
 const firebaseConfig = {
-  apiKey: "AIzaSyAKTyoitAc5hL5QHVtFQsZ5GKjDnox-8p4",
-  authDomain: "ecom-template-6777b.firebaseapp.com",
-  projectId: "ecom-template-6777b",
-  storageBucket: "ecom-template-6777b.firebasestorage.app",
-  messagingSenderId: "47381979458",
-  appId: "1:47381979458:web:65180a15792e16e49368db",
-  measurementId: "G-8ZPQXCXC53"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID, // optionnel
 };
 
-// Initialize Firebase
+// Initialisation de Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
